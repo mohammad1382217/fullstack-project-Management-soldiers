e973341254b4104090b2c6adcaa79f7556f18252
Mohammad Mohammadzade <95017872+mohammad1382217@users.noreply.github.com>
2022-12-25 14:16:48 +0330

    Delete visitor-panel.js

--- js/visitor-panel.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("addCompany").addEventListener("click", function () {
-    window.location.assign("./add-compani.html");
-})
-
-document.getElementsByClassName("compani-name")[0].addEventListener('click', function () {
-    window.location.assign("./compani-visits.html");
-})
